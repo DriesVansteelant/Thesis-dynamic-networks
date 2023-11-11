49223759b12b14268bcec4d125f0955ceb9e1d94
Dries Vansteelant <dries.vansteelant@qubris.be>
2023-11-11 16:54:54 +0100

    load from cvs

--- test_raphtory.py
+++ test_raphtory.py
@@ -2,19 +2,23 @@ from raphtory import Graph
 from raphtory import algorithms as algo
 import pandas as pd
 
-edges_df = pd.read_fwf("tglib/example_datasets/example_from_paper.tg", header=None)
+edges_df = pd.read_csv("tgbl-review_edgelist_v2.csv")
 # pd.read_csv('tglib/example_datasets/example_from_paper.tg', sep=" ", header=None)
 print("Edge Dataframe:")
 print(f"{edges_df}\n")
 
-# edges_df["timestamp"] = pd.to_datetime(edges_df["timestamp"]).astype(
-#     "datetime64[ms, UTC]"
-# )
+g = Graph.load_from_pandas(
+    edge_df=edges_df,
+    edge_src="source",
+    edge_dst="target",
+    edge_time="ts",
+    edge_props=["weight"],
+)
 
-# vertices_df = pd.read_csv("data/network_traffic_vertices.csv")
-# vertices_df["timestamp"] = pd.to_datetime(vertices_df["timestamp"]).astype(
-#     "datetime64[ms, UTC]"
-# )
+print("The resulting graphs and example vertex/edge:")
+print(g)
+print(g.vertex("2729"))
+print(g.edge("2729", "117010"))
 
 # print("Edge Dataframe:")
 # print(f"{edges_df}\n")
