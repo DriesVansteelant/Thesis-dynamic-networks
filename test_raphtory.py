1cfaa6ae23ad6acb6e54933ca9b73e63cc35e99b
Dries Vansteelant <dries.vansteelant@qubris.be>
2023-11-11 15:43:52 +0100

    wip

--- test_raphtory.py
+++ test_raphtory.py
@@ -2,52 +2,21 @@ from raphtory import Graph
 from raphtory import algorithms as algo
 import pandas as pd
 
-# Create a new graph
-graph = Graph()
+edges_df = pd.read_fwf("tglib/example_datasets/example_from_paper.tg", header=None)
+# pd.read_csv('tglib/example_datasets/example_from_paper.tg', sep=" ", header=None)
+print("Edge Dataframe:")
+print(f"{edges_df}\n")
 
-# Add some data to your graph
-graph.add_vertex(timestamp=1, id="Alice")
-graph.add_vertex(timestamp=1, id="Bob")
-graph.add_vertex(timestamp=1, id="Charlie")
-graph.add_edge(timestamp=2, src="Bob", dst="Charlie", properties={"weight": 5.0})
-graph.add_edge(timestamp=3, src="Alice", dst="Bob", properties={"weight": 10.0})
-graph.add_edge(timestamp=3, src="Bob", dst="Charlie", properties={"weight": -15.0})
+# edges_df["timestamp"] = pd.to_datetime(edges_df["timestamp"]).astype(
+#     "datetime64[ms, UTC]"
+# )
 
-# Check the number of unique nodes/edges in the graph and earliest/latest time seen.
-print(graph)
+# vertices_df = pd.read_csv("data/network_traffic_vertices.csv")
+# vertices_df["timestamp"] = pd.to_datetime(vertices_df["timestamp"]).astype(
+#     "datetime64[ms, UTC]"
+# )
 
-results = [["earliest_time", "name", "out_degree", "in_degree"]]
-
-# Collect some simple vertex metrics Ran across the history of your graph with a rolling window
-for graph_view in graph.rolling(window=1):
-    for v in graph_view.vertices:
-        results.append(
-            [graph_view.earliest_time, v.name, v.out_degree(), v.in_degree()]
-        )
-
-# Print the results
-print(pd.DataFrame(results[1:], columns=results[0]))
-
-# Grab an edge, explore the history of its 'weight'
-cb_edge = graph.edge("Bob", "Charlie")
-weight_history = cb_edge.properties.temporal.get("weight").items()
-print(
-    "The edge between Bob and Charlie has the following weight history:", weight_history
-)
-
-# Compare this weight between time 2 and time 3
-weight_change = cb_edge.at(2)["weight"] - cb_edge.at(3)["weight"]
-print(
-    "The weight of the edge between Bob and Charlie has changed by",
-    weight_change,
-    "pts",
-)
-
-# Run pagerank and ask for the top ranked node
-top_node = algo.pagerank(graph).top_k(1)
-print(
-    "The most important node in the graph is",
-    top_node[0][0],
-    "with a score of",
-    top_node[0][1],
-)
+# print("Edge Dataframe:")
+# print(f"{edges_df}\n")
+# print("Vertex Dataframe:")
+# print(f"{vertices_df}\n")
