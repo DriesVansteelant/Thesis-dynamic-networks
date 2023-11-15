da56e9c24cced2d0fefcad4d1a3c156171921051
Dries Vansteelant <dries.vansteelant@qubris.be>
2023-11-15 12:50:15 +0100

    15.11.1

--- test_raphtory.py
+++ test_raphtory.py
@@ -1,24 +1,87 @@
 from raphtory import Graph
 from raphtory import algorithms as algo
+import time 
+from tqdm import tqdm
 import pandas as pd
 
-edges_df = pd.read_csv("tgbl-review_edgelist_v2.csv")
-# pd.read_csv('tglib/example_datasets/example_from_paper.tg', sep=" ", header=None)
-print("Edge Dataframe:")
-print(f"{edges_df}\n")
+# start = time.time()
+# edges_df = pd.read_csv("tgbl-review_edgelist_v2.csv")
+# end = time.time()
+# print(f"Time to read pandas from file: {end - start}")
 
+# start = time.time()
+# g = Graph.load_from_pandas(
+#     edge_df=edges_df,
+#     edge_src="source",
+#     edge_dst="target",
+#     edge_time="ts",
+#     edge_props=["weight"],
+# )
+# end = time.time()
+
+# print(f"Time to load graph from pandas: {end - start}")
+
+
+start = time.time()
+edges_df = pd.read_csv("tglib/example_datasets/example_from_paper.tg", delimiter=" ")
+end = time.time()
+print(f"Time to read pandas from file: {end - start}")
+
+start = time.time()
 g = Graph.load_from_pandas(
     edge_df=edges_df,
-    edge_src="source",
-    edge_dst="target",
+    edge_src="s",
+    edge_dst="d",
     edge_time="ts",
-    edge_props=["weight"],
+    # edge_props=["weight"],
 )
+end = time.time()
 
+print(f"Time to load graph from pandas: {end - start}")
+
+start = time.time()
 print("The resulting graphs and example vertex/edge:")
 print(g)
-print(g.vertex("2729"))
-print(g.edge("2729", "117010"))
+print(g.vertex("0"))
+print(g.edge("0", "3"))
+end = time.time()
+
+print(f"Time to print random info: {end - start}")
+
+
+# stats
+
+
+start = time.time()
+num_nodes = g.count_vertices()
+num_edges = g.count_edges()
+min_in_deg = algo.min_in_degree(g)
+max_in_deg = algo.max_in_degree(g)
+min_out_deg = algo.min_out_degree(g)
+max_out_deg = algo.max_out_degree(g)
+min_ts = g.start
+max_ts = g.end
+# print(f"num_nodes: {num_nodes}")
+# print(f"num_edges: {num_edges}")
+# print(f"min_in_deg: {min_in_deg}")
+# print(f"max_in_deg: {max_in_deg}")
+# print(f"min_out_deg: {min_out_deg}")
+# print(f"max_out_deg: {max_out_deg}")
+# print(f"min_ts: {min_ts}")
+# print(f"max_ts: {max_ts}")
+
+end = time.time()
+
+print(f"Time to get stats: {end - start}")
+
+# print(g.vertices)
+nodes = [x.name for x in g.vertices] [0:10]
+print(nodes)
+# paths = algo.single_source_shortest_path(g, nodes)
+for s in tqdm(nodes):
+    path = algo.single_source_shortest_path(g, str(s))
+    print(path.to_string())
+
 
 # print("Edge Dataframe:")
 # print(f"{edges_df}\n")
