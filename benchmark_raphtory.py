59b3a50650d4f29706bbd21159941044f5bd75de
dlvsteel <Dries.Vansteelant@UGent.be>
2023-12-01 12:48:19 +0100

    pics

--- benchmark_raphtory.py
+++ benchmark_raphtory.py
@@ -55,6 +55,8 @@ class BenchmarkRaphtory():
 
 # Paths
         """..."""
+        algo.single_source_shortest_path(g,g.vertex(2265))
+
 
 # clustering coefficient
         start = time.time()
@@ -79,9 +81,9 @@ class BenchmarkRaphtory():
 
         return results
 
-# inPath = "../Code/Data/tgbl-wiki_edgelist_final.txt"
-# res = BenchmarkRaphtory.do_benchmark(inPath)
-# print(res)
+inPath = "../Code/Data/tgbl-wiki_edgelist_final.txt"
+res = BenchmarkRaphtory.do_benchmark(inPath)
+print(res)
 # inPath = "tgbl-review_edgelist_final.txt"
 # res = BenchmarkRaphtory.do_benchmark(inPath)
 # print(res)
